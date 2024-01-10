ccfb797deba4b3181c8ddf2fb3865241dd5ce920
LxzLLL <minepop09@163.com>
2024-01-10 23:25:17 +0800

    lodash

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,11 +1,33 @@
+/*
+ * @Author: LxzLLL
+ * @Date: 2024-01-10 21:22:21
+ * @LastEditors: LxzLLL
+ * @LastEditTime: 2024-01-10 22:54:26
+ * @FilePath: \ts-react-demo\src\components\box\index.tsx
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by 珠海虎眸创新科技有限公司, All Rights Reserved. 
+ */
 
+import { Button } from "antd";
 import styles from './box.module.scss'
 
 import { useState } from 'react'
+import _ from 'lodash'
 function Box(){
     // count 状态变量
     // setCount 修改状态变量的方法
     const [count,setCount] = useState(0)
+
+    const [form, setForm] = useState({name:'minepop'})
+    const changeForm = ()=>{
+        setForm({
+            ...form,
+            name: 'lxzlll'
+        })
+    }
+
+    const[list, setList] = useState([1,20,15,22,13,16])
 
     return (
         <div className={styles.box}>
@@ -13,6 +35,18 @@ function Box(){
             <button onClick={()=>setCount(count+1)}>+</button>
             <button onClick={()=>setCount(count-1)}>-</button>
             <p>{count}</p>
+
+            <p>useState对象变量</p>
+            <Button onClick={changeForm} type="primary">修改form的值{form.name}</Button>
+            <hr/>
+            <p>
+                <div>
+                    <Button type="primary" onClick={()=>setList(_.sortBy(list))}>升序排序</Button>
+                    <Button type="primary" onClick={()=>setList(_.sortBy(list,num=> -num))}>降序排序</Button>
+                </div>
+                
+                {list.map(item=><span>{item} </span>)}
+            </p>
         </div>
     )
 }
