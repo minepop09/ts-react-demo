d9268b0c40546feec6ce1e61e3ca778b79c0e41a
minepop <minepop09@163.com>
2024-01-10 17:11:01 +0800

    add box content

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,9 +1,18 @@
 
 import styles from './box.module.scss'
+
+import { useState } from 'react'
 function Box(){
+    // count 状态变量
+    // setCount 修改状态变量的方法
+    const [count,setCount] = useState(0)
+
     return (
         <div className={styles.box}>
             <p>这里是box的内容</p>
+            <button onClick={()=>setCount(count+1)}>+</button>
+            <button onClick={()=>setCount(count-1)}>-</button>
+            <p>{count}</p>
         </div>
     )
 }
